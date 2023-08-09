9a99b3ef5cd08e70ba0fca9b5f3aacfae4a19cad
rosstimo <rosstimo@isu.edu>
2023-08-09 13:17:30 -0600

    add relative distance calculations

--- Geo.vb
+++ Geo.vb
@@ -11,28 +11,34 @@ Module Geo
   Dim points as new List(Of (longitude$, latitude$, altitude$, R$, G$, B$))
   Dim maxPoint as (longitude$ , latitude$, altitude$, R$, G$, B$)
   Dim minPoint as (longitude$ , latitude$, altitude$, R$, G$, B$)
+'define bounding box
+  Dim mostWestSouthLowestPoint as (longitude$ , latitude$, altitude$, R$, G$, B$)
+  Dim mostEastNorthHighestPoint as (longitude$ , latitude$, altitude$, R$, G$, B$)
+ 
 
   Sub Main(args As String())
     for each arg in args
       Console.WriteLine(arg)
     next
     ' Console.WriteLine("Hello World!")
-    ' Console.WriteLine(CircumferenceAtLatitude(0,0))
-    ' Console.WriteLine(kmPerDegreeLongitudeAt(0,0))
-    ' Console.WriteLine(CircumferenceAtPoles(0))
-    ' Console.WriteLine(kmPerDegreeLatitude(0))
+    Console.WriteLine(CircumferenceAtLatitude(0,0))
+    Console.WriteLine(kmPerDegreeLongitudeAt(0,0))
+    Console.WriteLine(CircumferenceAtPoles(0))
+    Console.WriteLine(kmPerDegreeLatitude(0))
     ' Console.WriteLine(Convert.Test())
     ' Convert.ReadFileX()
     ReadFile("ranchPointAbsolute.txt")
     ' WritePoints()
-    GetMaximum()
+    GetMaxMinPoints()
     Console.WriteLine($"Max: {maxPoint.longitude} {maxPoint.latitude} {maxPoint.altitude} {maxPoint.R} {maxPoint.G} {maxPoint.B}")
     Console.WriteLine($"Min: {minPoint.longitude} {minPoint.latitude} {minPoint.altitude} {minPoint.R} {minPoint.G} {minPoint.B}")
+    Console.WriteLine($"Max: {maxPoint.longitude} {maxPoint.latitude} {maxPoint.altitude} {maxPoint.R} {maxPoint.G} {maxPoint.B}")
+    Console.WriteLine($"Min: {minPoint.longitude} {minPoint.latitude} {minPoint.altitude} {minPoint.R} {minPoint.G} {minPoint.B}")
+    relativeDistanceBetween(mostEastNorthHighestPoint, mostWestSouthLowestPoint)
   End Sub
 
   'this function returns the distance in km around the earth from east to west at a given latitude and optinal altitude in km
   Function CircumferenceAtLatitude(latitude As Double, optional altitude  as Double = 0) As Double
-    ' Dim pi as Double = pi
     Dim r As Double = (earthCircumferenceAtEquator/(2*pi)) + altitude
     Dim c as Double =  2 * pi * r * Math.Cos(latitude * (pi/180)) 'latitude in radians
     Return c
@@ -54,14 +60,40 @@ Module Geo
     return CircumferenceAtPoles(altitude) / 360
   End Function
 
+  Function kmFromPrimeMeridian(longitude as Double, latitude as Double, optional altitude as Double = 0) as Double
+    return CDbl(longitude * kmPerDegreeLongitudeAt(latitude, altitude))
+  End Function
+
+  Function kmFromEquator(latitude as Double, optional altitude as Double = 0) as Double
+    return CDbl(latitude * kmPerDegreeLatitude(altitude))
+  End Function
+
+  'this function returns the distance in meters between to longitude points with optional altitude
+  Function relativeDistanceBetween( currentPoint as (longitude$ , latitude$, altitude$, R$, G$, B$), _
+                                    referencePoint as (longitude$ , latitude$, altitude$, R$, G$, B$) _
+                                  ) as (x$ , y$, z$, R$, G$, B$)
+    Dim  relativePoint As (x$, y$, z$, R$, G$, B$)
+    relativePoint.x = CStr(((cDbl(currentPoint.longitude) - CDbl(referencePoint.longitude)) * kmPerDegreeLongitudeAt(referencePoint.latitude,referencePoint.altitude))*1000)
+    relativePoint.y = CStr(((CDbl(currentPoint.latitude) - CDbl(referencePoint.latitude)) * kmPerDegreeLatitude(referencePoint.altitude)) * 1000)
+    relativePoint.z = CStr(CDbl(currentPoint.altitude) - CDbl(referencePoint.altitude))   
+    relativePoint.R = currentPoint.R
+    relativePoint.G = currentPoint.G 
+    relativePoint.B = currentPoint.B
+
+    Console.WriteLine($"{currentPoint.longitude} {currentPoint.latitude} {currentPoint.altitude}")
+    Console.WriteLine($"{referencePoint.longitude} {referencePoint.latitude} {referencePoint.altitude}")
+    Console.WriteLine(((cDbl(currentPoint.longitude) - CDbl(referencePoint.longitude)) * kmPerDegreeLongitudeAt(referencePoint.latitude,referencePoint.altitude))*1000)
+    Console.WriteLine(((CDbl(currentPoint.latitude) - CDbl(referencePoint.latitude)) * kmPerDegreeLatitude(referencePoint.altitude)) * 1000)
+    Console.WriteLine(currentPoint.altitude - referencePoint.altitude)
+    return relativePoint 
+  End Function
+
+  'read the contents of the absolute latitude longitude points into a list of points
   Sub ReadFile(currentFile As String)
-    ' Dim currentFile as String = "ranchPointAbsolute.txt"
-    ' Dim currentRecord As String = ""
     Dim fileNumber As Integer = FreeFile()
     Dim temp As String 
     Dim longitude$ , latitude$, altitude$, R$, G$, B$
     Dim point = (longitude, latitude, altitude, R, G, B) 
-    ' Dim points as new List(Of (longitude$ , latitude$, altitude$, R$, G$, B$))
     Dim pointData() as String
 
     Try
@@ -96,7 +128,8 @@ Module Geo
   End Sub
 
   'determine the max/min longitude, latitude and altitude to establish a boundry reference for reletive points in 3D space
-  Sub GetMaximum()
+  'TODO this should probably be west most, sout most, lowest altitude to establish reletive origin point
+  Sub GetMaxMinPoints()
     Dim maxLongitude as Double = 0
     Dim minLongitude as Double = 360
     Dim maxLatitude as Double = 0
@@ -136,5 +169,17 @@ Module Geo
     minPoint.R = "0"
     minPoint.G = "0"
     minPoint.B = "0"
+    mostWestSouthLowestPoint.longitude = Cstr(maxLongitude)
+    mostWestSouthLowestPoint.latitude = Cstr(minLatitude)
+    mostWestSouthLowestPoint.altitude = Cstr(minAltitude)
+    mostWestSouthLowestPoint.R = "0"
+    mostWestSouthLowestPoint.G = "255"
+    mostWestSouthLowestPoint.B = "0"
+    mostEastNorthHighestPoint.longitude = Cstr(minLongitude)
+    mostEastNorthHighestPoint.latitude = Cstr(maxLatitude)
+    mostEastNorthHighestPoint.altitude = Cstr(maxAltitude)
+    mostEastNorthHighestPoint.R = "255"
+    mostEastNorthHighestPoint.G = "0"
+    mostEastNorthHighestPoint.B = "0"
   End Sub
 End Module
